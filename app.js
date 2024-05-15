30fddbf39fcc22d01658bfa01f85a9052c4b9223
coderabbit-amank <aman@coderabbit.ai>
2024-05-15 17:16:55 +0530

    Episode - 3 Laying the foundation

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-/**
- * <div id = "parent">
- *    <div id = "child">
- *      <h1>Hello from h1 tag !</h1>
- *      <h2>Hello from h2 tag !</h2>
- *    </div>
- * </div>
- *
- */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const h1 = React.createElement("h1", { key: 1 }, "Hello from h1 tag !");
-
-const h2 = React.createElement("h2", { key: 2 }, "Hello from h2 tag !");
-
-const child1 = React.createElement("div", { id: "child1", key: 1 }, [h1, h2]);
-
-const child2 = React.createElement("div", { id: "child2", key: 2 }, [h1, h2]);
-
-const prt = React.createElement("div", { id: "parent" }, [child1, child2]);
+const h1 = <h1>Namaste React from Akshay Saini</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(prt);
+
+// React Components
+
+const Title = () => {
+  return <h1>Namaste React using JSX🗿</h1>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h1>Namaste react functional Components! by Akshay Saini</h1>
+    </div>
+  );
+};
+
+root.render(<HeadingComponent />);
